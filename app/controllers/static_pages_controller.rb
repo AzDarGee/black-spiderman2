f6543305b41a025662e50cbd6eb5b0d462cd6d03
AzDarGee <ashishdarji88@gmail.com>
2014-09-24 16:20:46 -0400

    updated app

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,20 @@
+class StaticPagesController < ApplicationController
+	def index
+	end
+	def the_boy
+	end
+	def donald_glover
+	end
+	def childish_gambino
+	end
+	def troy
+	end
+	def unlabeled
+	end
+	def real
+	end
+	def home
+	end
+	def about
+	end
+end
---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  resources :pins
+
+  devise_for :users
+  root :to => "static_pages#index"
+  get 'about' => "pages#about"
+
+  # static pages
+  get '/the-boy' => "static_pages#the_boy", as: :theboy
+  get '/childishgambino' => "static_pages#childish_gambino", as: :childishgambino
+  get '/donaldglover' => "static_pages#donald_glover", as: :donaldglover
+  get '/troy' => "static_pages#troy", as: :troy
+  get '/unlabeled' => "static_pages#unlabeled", as: :unlabled
+  get '/real' => "static_pages#real", as: :real
+  
+end
