b406a4639d2c4ac5a40ce277d6738464fb73d980
AzDarGee <ashishdarji88@gmail.com>
2014-09-24 17:49:11 -0400

    k

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,4 +16,68 @@
 //= require jquery.turbolinks
 //= require turbolinks
 //= require masonry/jquery.masonry
+//= require masonry/jquery.imagesloaded.min
+
 //= require_tree .
+
+$(document).on('page:load',function() {
+
+ //    var origHeight = $("div").outerHeight(true);
+
+	// $("div").hover(function(e){
+	//     $(this).stop().animate({
+	//         height: e.type === "mouseenter" ? $(window).height() : origHeight
+	//     });
+	// });
+
+
+
+	//Top navbar fixed
+
+	// var smallWindow = false;
+
+	// $(window).scroll(function() {
+	// 	var scroll = $(window).scrollTop();
+
+	// 	if (scroll >= 50) {
+	// 		//$('#logo-image').attr('src', 'img/Acme_Monogram_Colour.png')
+	// 		$(".important-class").addClass("padding-on-my-header");
+	// 	}
+	// 	if (scroll < 50) {
+	// 		$(".important-class").removeClass("padding-on-my-header");
+	// 		//$('#logo-image').attr('src', 'img/Acme_Colour.png')
+	// 	}
+	// }).resize(function(){
+	// 	if ( !smallWindow && this.innerWidth <= 1024 ) {
+	// 		smallWindow = true;
+	// 		$('.top-bar-section').find('ul.right').hide(0).delay(500).show(0);
+	// 	}
+	// 	if ( smallWindow && this.innerWidth > 1024 ) {
+	// 		smallWindow = false;
+	// 	}
+	// });
+
+	// var columns    = 3,
+ //        setColumns = function() { columns = $( window ).width() > 640 ? 3 : $( window ).width() > 320 ? 2 : 1; };
+ 
+ //    setColumns();
+ //    $( window ).resize( setColumns );
+ 
+ //    $( '#list' ).masonry(
+ //    {
+ //        itemSelector: '.card',
+ //        columnWidth:  function( containerWidth ) { return containerWidth / columns; }
+ //    });
+
+
+// $(function() {
+//   return $('#list').imagesLoaded(function() {
+//     return $('#list').masonry({
+//       itemSelector: '.card',
+//       isFitWidth: true
+//     });
+//   });
+// });
+
+
+});
