802ce01af0bc3ab3058be8f958c76b1cdc88eb55
AzDarGee <ashishdarji88@gmail.com>
2014-09-24 21:16:38 -0400

    updated

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,6 +2,6 @@ class Pin < ActiveRecord::Base
 	belongs_to :user
 	has_attached_file :image, :styles => { :thumb => "100x100>", :medium => "300x300>", :large => "500x500>", :xlarge => "800x800>", :xxlarge => "960x960>", :xxxlarge => "1200x1200>" }
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
-  validates :description, presence: true
+  validates :title, presence: true
   validates :image, presence: true
 end
